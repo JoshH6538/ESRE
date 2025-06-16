e406691045b7f68ed9d612677d449badd067ae5e
JoshH6538 <JOSHUAHANSCOM6538@GMAIL.COM>
2025-06-16 10:20:05 -0700

    Pushing cosmetic changes

--- js/display-realtors.js
+++ js/display-realtors.js
@@ -1,4 +1,4 @@
-import { allRealtors } from "./retool-api.js";
+import { allRealtorUsers } from "./retool-api.js";
 
 let allRealtorsData = [];
 
@@ -6,7 +6,7 @@ let allRealtorsData = [];
 // allRealtorsData is updated with the full list of realtors
 async function loadRealtors() {
   console.log("Loading realtors...");
-  allRealtorsData = await allRealtors(); // Save the full list
+  allRealtorsData = await allRealtorUsers(); // Save the full list
   console.log("Realtors loaded:", allRealtorsData);
   renderRealtors(allRealtorsData);
   updateResultsCount(allRealtorsData.length);
@@ -18,24 +18,35 @@ function renderRealtors(data) {
 
   //for each realtor, create a column and append it to the container
   data.forEach((realtor) => {
+    console.log("ICON URL:", realtor.iconURL);
+
     const col = document.createElement("div");
     col.className = "col-xl-3 col-md-4 col-sm-6";
 
     col.innerHTML = `
       <div class="agent-card-two position-relative z-1 mb-50 wow fadeInUp">
-        <div class="media bg-dark position-relative overflow-hidden">
+        <div class="media ${
+          realtor["iconURL"] ? "bg-white" : "bg-dark"
+        } position-relative overflow-hidden">
+
           <div class="tag bg-white position-absolute text-uppercase">DRE #: ${
-            realtor["DRE #"] ?? "DRE #"
+            realtor["dre"] ?? "DRE #"
           }</div>
-          <img src="images/lazy.svg" data-src="images/agent/img_07.jpg" alt="" class="lazy-img agent-img w-100 tran5s">
+          <img
+  loading="lazy"
+  src="${realtor["iconURL"] ?? "images/lazy.svg"}"
+  class="agent-img w-100 tran5s"
+  alt=""
+>
+
         </div>
         <div class="text-center pt-30">
           <h6 class="name">
-            <a href="agent_details.html">${realtor["First Name"] ?? "First"} ${
-      realtor["Last Name"] ?? "Last"
+            <a href="agent_details.html">${realtor["firstName"] ?? "First"} ${
+      realtor["lastName"] ?? "Last"
     }</a>
           </h6>
-          <div class="designation">${realtor["Title"] ?? "Agent"}</div>
+          <div class="designation">${realtor["title"] ?? "Agent"}</div>
         </div>
       </div>
     `;
@@ -58,16 +69,16 @@ document.getElementById("realtorSearchForm").addEventListener("submit", (e) => {
     .toLowerCase();
 
   const filtered = allRealtorsData.filter((realtor) => {
-    // Combine first name, last name, and convert to lowercase
-    const name = `${realtor["First Name"] ?? ""} ${
-      realtor["Last Name"] ?? ""
+    // Combine first name, lastName, and convert to lowercase
+    const name = `${realtor["firstName"] ?? ""} ${
+      realtor["lastName"] ?? ""
     }`.toLowerCase();
     // Convert DRE # and Zipcode to lowercase
-    const dre = (realtor["DRE #"] ?? "").toLowerCase();
-    const zip = (realtor["Zipcode"] ?? "").toLowerCase(); // new Zipcode field
+    const dre = (realtor["dre"] ?? "").toLowerCase();
+    // const zip = (realtor["zipcode"] ?? "").toLowerCase(); // new Zipcode field
 
     // Return true if any of the fields match the query
-    return name.includes(query) || dre.includes(query) || zip.includes(query);
+    return name.includes(query) || dre.includes(query);
   });
   renderRealtors(filtered);
   //   If no realtors match the search, display a message
--- js/retool-api.js
+++ js/retool-api.js
@@ -44,3 +44,23 @@ export async function allRealtors() {
     return [];
   }
 }
+
+export async function allRealtorUsers() {
+  try {
+    const response = await fetch(BASE_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        type: "allRealtorUsers",
+      }),
+    });
+
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.error("Error fetching all realtors:", err);
+    return [];
+  }
+}
